713996508ab420f1b6402b57617e6f0a935c643e
jackutea <chenwansal1@163.com>
2024-06-05 21:04:38 +0800

    <feature> add: 值拷贝错误示例

--- src/main.c
+++ src/main.c
@@ -2,25 +2,31 @@
 #include "Context.h"
 #include <stdio.h>
 
-void ProcssInput(Context* ctx) {
-
+void Fake_ChangeValue(int input) {
+    input = 5;
 }
+
+void Real_ChangeValue(int* input) {
+    *input = 5;
+}
+
+void ProcssInput(Context *ctx) {}
 
 // *ctx 表示它是一个指针, 也即是地址
-void DoLogic(Context* ctx, int* value, float dt) {
+void DoLogic(Context *ctx, float dt) {
     // 传入了地址
     ctx->moveX = 5;
-    *value = 20;
 }
 
-void Draw(Context* ctx) {
-
-}
+void Draw(Context *ctx) {}
 
 int main(void) {
 
     Context ctx; // 1000
     int a = 10;
+    Fake_ChangeValue(a);
+    // Real_ChangeValue(&a);
+    printf("a: %d\n", a);
 
     // 初始化窗口
     InitWindow(800, 450, "raylib [core] example - basic window");
@@ -46,9 +52,7 @@ int main(void) {
         // ==== 2.2 DoLogic 处理逻辑 ====
         // 不可见的数据变化
         // 根据 move 移动小球
-        DoLogic(&ctx, &a, deltaTime);
-        printf("moveX: %f\n", ctx.moveX);
-        printf("a: %d\n", a);
+        DoLogic(&ctx, deltaTime);
 
         BeginDrawing();
 
@@ -60,7 +64,7 @@ int main(void) {
         DrawFPS(10, 10);
 
         // ==== 2.3 Draw 表现(渲染) ====
-        // `&ctx` 地址 
+        // `&ctx` 地址
         // `ctx` 值
         Draw(&ctx);
 
