879a5058f6cc7afc41b0e2d9320fcc05853acb94
jackutea <chenwansal1@163.com>
2024-06-10 20:31:49 +0800

    <feature> add: 指针、变量、指针的指针、解引用

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
 #include "../include/raylib.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
+
+    // 变量必有两个属性: 地址, 值
+    // 0x1000 = 0000 0000 | 0000 0000 | 0000 0000 | 0000 0011 = 3
+    // 4byte = 32bit
+    int a = 3; // 栈 32G --
+
+    int size = 16; // 16 byte
+    
+    // pointer 也是变量: 地址, 值(存的是别人的地址)
+    // 0x500 = 0x1000
+    int* aptr = &a;
+    int value = *aptr; // 0x500 ->(*) 0x1000 -> 3
+
+    // 0x600 = 0x500
+    int** apptr = &aptr;
+    int v2 = **apptr; // 0x600 ->(*) 0x500 ->(*) 0x1000 -> 3
+
+    void* pointer = malloc(size); // 堆 0G ++
 
     InitWindow(800, 600, "Game");
 
